271187c2035141d804f85925a6ecd2376bf1d4ae
Fran Melo <francisco.melo@tiendanube.com>
2022-07-14 15:22:17 -0300

    Fix fetch.

--- public/scripts/quiz.js
+++ public/scripts/quiz.js
@@ -1,4 +1,7 @@
 // DEFINICION DE VALORES CONSTANTES
+
+const jsonUrl = '../src/utils/answers.json';
+
 const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
@@ -6,101 +9,103 @@ const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
 
 // DEFINICION DE VALORES VARIABLES
-let currentQuestion = {}
-let acceptingAnswers = true
-let score = 0
-let questionCounter = 0
-let availableQuestions = []
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
 
 // Preguntas para "QuizScript"
-const jsonUrl = '../src/utils/answers.json';
-
 function getData(url) {
-return fetch(url)
-    .then((res) => res.json())
-    .then((data) => data);
+    return fetch(url)
+        .then((res) => res.json())
+        .then((data) => data);
 }
 
 function getAnswers() {
-getData(jsonUrl).then((data) => {
-questions = data;
-});
+    return getData(jsonUrl).then((data) => data);
 }
 
-let questions;
-getAnswers();
-
-const SCORE_POINTS = 100 // Seteo del valor para cada pregunta correcta
-const MAX_QUESTIONS = 4 // Setelo del valor máximo de preguntas a mostrar
+const SCORE_POINTS = 100; // Seteo del valor para cada pregunta correcta
+const MAX_QUESTIONS = 4; // Setelo del valor máximo de preguntas a mostrar
 
 // INIICO DEL JUEGO
-startGame = () => {
-    questionCounter = 0
-    score = 0
+startGame = async () => {
+    const questions = await getAnswers();
+    questionCounter = 0;
+    score = 0;
     const queryString = window.location.search;
     console.log(queryString);
     const urlParams = new URLSearchParams(queryString);
-    const category = urlParams.get('category')
-    availableQuestions = [...questions[category]]
-    getNewQuestion()
-    console.log(availableQuestions)
-}
+    const category = urlParams.get('category');
+    console.log(questions);
+    availableQuestions = [...questions[category]];
+    getNewQuestion();
+    console.log(availableQuestions);
+};
 
 // Trackeo y busqueda de las preguntas
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
 
-        return window.location.assign('/pages/ending-quiz.html')
+        return window.location.assign('/pages/ending-quiz.html');
     }
 
-    questionCounter++
-    progressText.innerText = `Pregunta ${questionCounter} de ${MAX_QUESTIONS}` // Indice de preguntas
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%` // Obtencion del puntaje
+    questionCounter++;
+    progressText.innerText = `Pregunta ${questionCounter} de ${MAX_QUESTIONS}`; // Indice de preguntas
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`; // Obtencion del puntaje
 
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length) // Seteo de orden random para mostrar las preguntas
-    currentQuestion = availableQuestions[questionsIndex]
-    question.innerText = currentQuestion.question
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length); // Seteo de orden random para mostrar las preguntas
+    currentQuestion = availableQuestions[questionsIndex];
+    question.innerText = currentQuestion.question;
 
-    choices.forEach(choice => { // Obtencion de la respuesta correcta
-        const number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice' + number]
-    })
+    choices.forEach((choice) => {
+        // Obtencion de la respuesta correcta
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
 
-    availableQuestions.splice(questionsIndex, 1)
+    availableQuestions.splice(questionsIndex, 1);
 
-    acceptingAnswers = true
-}
+    acceptingAnswers = true;
+};
 
-choices.forEach(choice => { // Identificacion de click en respuesta correcta o incorrecta
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return
+choices.forEach((choice) => {
+    // Identificacion de click en respuesta correcta o incorrecta
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
 
-        acceptingAnswers = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset['number']
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
 
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct-answer' : 'incorrect-answer' // Dinamismo de clases
+        let classToApply =
+            selectedAnswer == currentQuestion.answer
+                ? 'correct-answer'
+                : 'incorrect-answer'; // Dinamismo de clases
 
-        if(classToApply === 'correct-answer') { // Aumento de puntos si la respuesta es correcta
-            incrementScore(SCORE_POINTS)
+        if (classToApply === 'correct-answer') {
+            // Aumento de puntos si la respuesta es correcta
+            incrementScore(SCORE_POINTS);
         }
 
-        selectedChoice.parentElement.classList.add(classToApply)
+        selectedChoice.parentElement.classList.add(classToApply);
 
         setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
-        }, 1000)
-    })
-})
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+    });
+});
 
-incrementScore = num => { // Obtencion del puntaje
-    score += num
-    scoreText.innerText = score
-}
+incrementScore = (num) => {
+    // Obtencion del puntaje
+    score += num;
+    scoreText.innerText = score;
+};
 
 /**********************************/
 /* LLAMADA A LA FUNCION PRINCIPAL */
 /**********************************/
-startGame()
+startGame();
