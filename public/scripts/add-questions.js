ad9efb2fb621b5234895c04de6520c4f1d3684ee
MeloCreativo <Franciscomelo32@gmail.com>
2022-07-26 20:28:08 -0300

    Add form logic.

--- public/scripts/add-questions.js
+++ public/scripts/add-questions.js
@@ -20,37 +20,49 @@ const getFormData = (e) => {
 }
 document.querySelector("[type='submit']").addEventListener('click', getFormData)
 
-const submitBtn = document.getElementById('save')
-
-const newQuestion = document.getElementById('new-question')
-const correctAnswer = document.getElementById('correct-answer')
-const incorrectAnswer1 = document.getElementById('incorrect-answer1')
-const incorrectAnswer2 = document.getElementById('incorrect-answer2')
-const incorrectAnswer3 = document.getElementById('incorrect-answer3')
-const selectCategories = document.getElementById('select-categories')
-
-// run this function whenever the values of any of the above 4 inputs change.
-// this is to check if the input for all 4 is valid.  if so, enable submitBtn.
-// otherwise, disable it.
-const checkEnableButton = () => {
-  submitBtn.disabled = !(
-      newQuestion.value && 
-      correctAnswer.value && 
-      incorrectAnswer1.value &&
-      incorrectAnswer2.value &&
-      incorrectAnswer3.value &&
-      selectCategories.value &&
-      selectCategories.value !== 'Choose'
-   )
+function inputCheck() {
+    const submitBtn = document.getElementById('save')
+    
+    const newQuestion = document.getElementById('new-question')
+    const correctAnswer = document.getElementById('correct-answer')
+    const incorrectAnswer1 = document.getElementById('incorrect-answer1')
+    const incorrectAnswer2 = document.getElementById('incorrect-answer2')
+    const incorrectAnswer3 = document.getElementById('incorrect-answer3')
+    const selectCategories = document.getElementById('select-categories')
+    
+    const checkEnableButton = () => {
+      submitBtn.disabled = !(
+          newQuestion.value && 
+          correctAnswer.value && 
+          incorrectAnswer1.value &&
+          incorrectAnswer2.value &&
+          incorrectAnswer3.value &&
+          selectCategories.value &&
+          selectCategories.value !== 'Choose'
+       )
+    }
+    
+    newQuestion.addEventListener('change', checkEnableButton)
+    correctAnswer.addEventListener('change', checkEnableButton)
+    incorrectAnswer1.addEventListener('change', checkEnableButton)
+    incorrectAnswer2.addEventListener('change', checkEnableButton)
+    incorrectAnswer3.addEventListener('change', checkEnableButton)
+    selectCategories.addEventListener('change', checkEnableButton)
 }
 
-newQuestion.addEventListener('change', checkEnableButton)
-correctAnswer.addEventListener('change', checkEnableButton)
-incorrectAnswer1.addEventListener('change', checkEnableButton)
-incorrectAnswer2.addEventListener('change', checkEnableButton)
-incorrectAnswer3.addEventListener('change', checkEnableButton)
-selectCategories.addEventListener('change', checkEnableButton)
+inputCheck();
 
+function myFunc() {
+    document.getElementById('add-question').reset();
+    document.getElementById("hidden_div").style.display = "block";
+    document.getElementById("add-question").onsubmit = function(){
+        location.reload(true);
+    }
+}
+
+function showDiv() {
+    document.getElementById("hidden_div").style.display = "block";
+}
 
 var serializeForm = function (form) {
 	var obj = {};
